3f2ff55b76db48c5bc7ef23316e5a519e093e207
LucasCurtyRM <lucas.curty.r.matheus@gmail.com>
2023-09-19 10:47:01 -0300

    (issue #0) feat: Adicionado os métodos da Carta

--- src/types/Carta/Carta.h
+++ src/types/Carta/Carta.h
@@ -1,10 +1,21 @@
-enum Posicao{
+typedef enum{
     CIMA,
     BAIXO
-};
+}Posicao;
 
 typedef struct{
     char naipe;
     int valor;
     Posicao posicao;
-}Cartas;
+}Carta;
+
+void criarCarta(Carta* carta, char naipe, int valor, Posicao posicao);
+char getNaipe(Carta* carta);
+int getValor(Carta* carta);
+Posicao getPosicao(Carta* Carta);
+void setPosicao(Carta* carta);
+
+int compararNaipesIguais(Carta* carta1, Carta* carta2);
+int compararNaipesDiferentes(Carta* carta1, Carta* carta2);
+
+void exibirCarta(Carta* carta);
