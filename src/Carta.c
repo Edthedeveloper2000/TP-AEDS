abc68e7205df5a0ba029dad9ba6e6b60e8d5d478
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2023-09-19 21:01:36 -0300

    (issue #0) refactor: movidos Carta.h e Carta.c para a raiz e adicionados métodos `compararNaipesDiferentes` e ` exibirCarta`

---
+++ src/Carta.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "Carta.h"
+
+void criarCarta(Carta *carta, char naipe, int valor, Posicao posicao) {
+    carta->naipe = naipe;
+    carta->posicao = posicao;
+    carta->valor = valor;
+}
+
+char getNaipe(Carta *carta) {
+    return carta->naipe;
+}
+
+int getValor(Carta *carta) {
+    return carta->valor;
+}
+
+Posicao getPosicao(Carta *carta) {
+    return carta->posicao;
+}
+
+void setPosicao(Carta *carta) {
+    carta->posicao = !carta->posicao;
+}
+
+int compararNaipesIguais(Carta *carta1, Carta *carta2) {
+    if(!carta1) {
+        if(carta2->valor==1){
+            return 1;
+        } else {
+            return 0;
+        }
+    } 
+    
+    else if (carta1->naipe == carta2->naipe && carta2->valor == (carta1->valor + 1)) {
+        return 1;
+    }
+
+    return 0;
+} 
+
+int compararNaipesDiferentes( Carta *carta1, Carta *carta2){
+    if(!carta1) {
+        if(carta2->valor==13){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if(carta1->naipe == 'c' || carta1->naipe == 'o') {
+        if(carta2->naipe == 'p' || carta2->naipe == 'e' ) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else if (carta1->naipe =='p' || carta1->naipe == 'e') {
+        if(carta2->naipe == 'c' || carta2->naipe == 'o') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+void exibirCarta(Carta *carta) {
+    switch (carta->naipe){
+    case 'c':
+        printf("[♥️ %d]", carta->valor);
+        break;
+
+    case 'p' :
+        printf("[♣️ %d]", carta->valor);
+        break;
+        
+    case 'e' :
+        printf("[♠️ %d]", carta->valor);
+        break;
+
+    case 'o' :
+        printf("[♦️ %d]", carta->valor);
+        break;
+        
+    default:
+        break;
+    }
+}
