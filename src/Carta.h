faf7fd5c6d714b5b22f8073fe1e72403a8962de5
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2023-09-19 21:50:21 -0300

    (issue #0) style: corrifida formatação do código

--- src/Carta.h
+++ src/Carta.h
@@ -9,13 +9,13 @@ typedef struct{
     Posicao posicao;
 }Carta;
 
-void criarCarta(Carta* carta, char naipe, int valor, Posicao posicao);
-char getNaipe(Carta* carta);
-int getValor(Carta* carta);
-Posicao getPosicao(Carta* Carta);
-void setPosicao(Carta* carta);
+void criarCarta(Carta *carta, char naipe, int valor, Posicao posicao);
+char getNaipe(Carta *carta);
+int getValor(Carta *carta);
+Posicao getPosicao(Carta *Carta);
+void setPosicao(Carta *carta);
 
-int compararNaipesIguais(Carta* carta1, Carta* carta2);
-int compararNaipesDiferentes(Carta* carta1, Carta* carta2);
+int compararNaipesIguais(Carta *carta1, Carta *carta2);
+int compararNaipesDiferentes(Carta *carta1, Carta *carta2);
 
-void exibirCarta(Carta* carta);
+void exibirCarta(Carta *carta);
