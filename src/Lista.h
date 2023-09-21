aee879ba31f00f1f71ce63f1b3bcaa45e61078d3
LucasCurtyRM <lucas.curty.r.matheus@gmail.com>
2023-09-21 10:40:38 -0300

    (issue #0) fix: modificado o retorno das funções para de int void

--- src/Lista.h
+++ src/Lista.h
@@ -21,7 +21,7 @@ Carta getCarta(Lista *lista, int posicao);
 void addCartaAoTopo(Lista *lista, Carta *carta);
 
 int retirarCartaDoTopo(Lista *lista, Carta *carta);
-int transferirCartas(Lista *lista1, Lista *lista2, int quantidade);
-int embaralhar(Lista *lista);
+void transferirCartas(Lista *lista1, Lista *lista2, int quantidade);
+void embaralhar(Lista *lista);
 
 void exibirLista(Lista* lista, int mostrarTodas);
