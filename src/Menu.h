aceb7502cd447886102c21400c878cdbfe4dd066
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2023-09-30 13:11:08 -0300

    (issue #0) refactor: removido método não utilizando  `AcaoMoverDescarteParaBase`

--- src/Menu.h
+++ src/Menu.h
@@ -5,6 +5,5 @@ void IniciarModoInterativo();
 void executarAcao(Mesa *mesa,  int opcao);
 void AcaoMoverEntreColunasDoTableau(Mesa *mesa);
 void AcaoMoverDescarteParaTableau(Mesa *mesa);
-void AcaoMoverDescarteParaBase(Mesa *mesa);
 void AcaoMoverDoTableauParaBase(Mesa *mesa);
 void AcaoMoverDaBaseParaOTableau(Mesa *mesa);
